5e35d77dcc56a711e556edcafbf3797cb7577a17
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2023-04-18 10:43:49 +0530

    integrating jwt (authentication) with app

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ import models
 from resources.item import blp as ItemBlueprint
 from resources.brand import blp as BrandBlueprint
 from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
+
+import secrets
+from flask_jwt_extended import JWTManager
 
 def create_app(db_url=None):
     app = Flask(__name__)
@@ -33,6 +37,10 @@ def create_app(db_url=None):
     db.init_app(app)
     api = Api(app)
 
+    # integrating jwt (authentication) with app
+    app.config["JWT_SECRET_KEY"] = "106561910718341680690669917170099921986"
+    jwt = JWTManager(app)
+
     # creating all the table in db if not already created
     with app.app_context():
         db.create_all()
@@ -42,5 +50,6 @@ def create_app(db_url=None):
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(BrandBlueprint)
     api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
 
     return app
