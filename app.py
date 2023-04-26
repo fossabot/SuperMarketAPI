0b5622976f750442d8c0bc14fb8a3e03ce0b45e6
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2023-04-26 11:48:12 +0530

    Added swagger-ui api documentation

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def create_app(db_url=None):
     app = Flask(__name__)
 
     # Adding configurations to application
-    app.config["API_TITLE"] = "BigBazaar SuperMarket REST API"
+    app.config["API_TITLE"] = "Khazana SuperMarket REST API"
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
     app.config["OPENAPI_URL_PREFIX"] = "/"
