35ee6d8b00c88e4abf0f67def3a73b9620459ee4
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2023-04-26 10:10:57 +0530

    Added login functionality

--- resources/user.py
+++ resources/user.py
@@ -3,6 +3,7 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
+from flask_jwt_extended import create_access_token
 
 from db import db
 from models import UserModel
@@ -21,7 +22,7 @@ class UserRegister(MethodView):
 
         user = UserModel(
             username = user_data["username"],
-            password = pbkdf2_sha256.hash(user_data["username"])
+            password = pbkdf2_sha256.hash(user_data["password"])
         )
         db.session.add(user)
         db.session.commit()
@@ -30,8 +31,21 @@ class UserRegister(MethodView):
         # return {"message" : "User created successfully."}
 
 
-@blp.route("/user/<int:user_id>")
+@blp.route("/login")
 class UserLogin(MethodView):
+
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        user = UserModel.query.filter(UserModel.username == user_data["username"]).first()
+
+        if user and pbkdf2_sha256.verify(user_data["password"], user.password):
+            access_token = create_access_token(identity=user.id, additional_claims=user_data["username"])
+            return {"access_token" : access_token}
+
+        abort(401, message="Invalid credentials")
+
+@blp.route("/user/<int:user_id>")
+class User(MethodView):
 
     @blp.response(200, UserSchema)
     def get(self, user_id):
